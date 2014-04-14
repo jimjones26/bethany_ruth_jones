7efe505a1b1e5f0c7962314f51313103777622a6
Jimmy Jones <jimjones26@gmail.com>
2014-04-14 19:47:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BethanyRuthJones::Application.config.secret_key_base = 'fb6216294d86108cfb3341092807ea9b23dd574ea95872440e3c22a1d2c7816c5a103710a9373f5d792ae140edf9986aa0fc5ff83fdaf893aa102eb4e827f0d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BethanyRuthJones::Application.config.session_store :cookie_store, key: '_bethany_ruth_jones_session'
