82964d1acdce423df2ef316480e88bbea938c483
Jimmy Jones <jimjones26@gmail.com>
2014-04-19 01:45:14 -0400

    add basic breadcrumb functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,35 @@ module ApplicationHelper
 			"#{base_title} | #{page_title}"					# string interpolation
 		end
 	end
-end
+
+	# creates breadcrumbs
+	def breadcrumb_str options
+		items = []
+		char_sep = "".html_safe
+		if(options.size !=0 )
+			items <<  content_tag(:li , :class => "active") do
+				link_to("Home", root_path) + content_tag(:span, char_sep, :class => "divider")
+			end
+			options.each do |option|
+				option.each do |key, value|
+					if !value.nil?
+						items << content_tag(:li) do 						
+							link_to(key, value) + content_tag(:span, char_sep, :class => "divider")
+						end
+					else
+						items << content_tag(:li, key, :class =>"active")
+					end
+				end
+			end
+		else
+			items << content_tag(:li, "Home", :class => "active")
+		end
+
+		items.join("").html_safe
+	end
+
+	def breadcrumb options
+		content_tag(:ol, breadcrumb_str(options), :class => "breadcrumb pull-right hidden-xs")
+	end
+
+  end
