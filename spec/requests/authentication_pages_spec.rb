37ae443c4324dadf0b180b458706d067f87ed604
Jimmy Jones <jimjones26@gmail.com>
2014-04-26 02:11:20 -0400

    add user list

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,10 +35,12 @@ describe "Authentication" do
 				click_button "Sign in"
 			end
 
-			#it { should have_title(user.name) }
-			it { should have_link('Profile',		href: user_path(user)) }
-			it { should have_link('Sign out',		href: signout_path) }
-			it { should_not have_link('Sign in',	href: signin_path) }
+			#it { should have_title(full_title(user.first_name)) }
+			it { should have_link('Users',       href: users_path) }
+			it { should have_link('Profile',     href: user_path(user)) }
+			it { should have_link('Settings',    href: edit_user_path(user)) }
+			it { should have_link('Sign out',    href: signout_path) }
+			it { should_not have_link('Sign in', href: signin_path) }
 
 			describe "followed by signout" do
 				before { click_link('Sign out') }
@@ -79,6 +81,11 @@ describe "Authentication" do
 				describe "submitting to the update action" do
 					before { patch user_path(user) }
 					specify { expect(response).to redirect_to(signin_path) }
+				end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_title(full_title('Sign in')) }
 				end
 			end
 		end
